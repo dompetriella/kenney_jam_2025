2a2fed945f12cdd5d02537e4fe2a11959d584542
Dominic Petriella <dompetriella@gmail.com>
2025-07-19 11:21:08 -0400

    Added interact area and color change

--- scenes/world/interact_area/interact_area.gd
+++ scenes/world/interact_area/interact_area.gd
@@ -1,1 +1,19 @@
 extends Area2D
+
+enum InteractType {
+	ADD_TO_INVENTORY,
+	DIALOGUE
+}
+
+@export var interact_type: InteractType;
+
+var player_in_area: bool = false;
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body is Player):
+		Locator.get_player().set_is_in_interactable_area(true);
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if (body is Player):
+		Locator.get_player().set_is_in_interactable_area(false);
