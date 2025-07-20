729ea6aa561385137c3abb95160747fedd50d67a
Dustin Thurston <dustin@dustycode.dev>
2025-07-20 09:59:07 -0400

    win menu & hooked up blue gate properly

---
+++ scenes/menus/win_menu/win_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var title_menu = load("uid://l6xve34c75ym");
+var dudes_killed = Locator.get_dude_manager().dudes_lost
+
+func _ready() -> void:
+	GameMessenger.change_camera_focus.emit(CameraType.Focus.MENU)
+	if dudes_killed > 0:
+		$MarginContainer/VBoxContainer/dude_count.text = str(dudes_killed) + " dudes. You are a monster."
+	else:
+		$MarginContainer/VBoxContainer/dude_count.text = "...actually, it turns out you didn't kill any dudes. You're a good wizard!"
+
+	
+	
+func _on_play_again_button_pressed() -> void:
+	Locator.get_dude_manager().reset_dudes()
+	Locator.get_scaffold().scaffold_new_node_tree(title_menu.instantiate());
